a8f696302bf95192f142e2f0ea9ef3ab258580ed
Dell <Dell@DESKTOP-7PH7RQE>
2026-04-20 22:26:31 +0545

    Session

---
+++ src/main/java/com/sessiontracking/SessionTrackingServlet.java
@@ -0,0 +1,54 @@
+package com.sessiontracking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/SessionTracker")
+public class SessionTrackingServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Create or get existing session
+        HttpSession session = request.getSession(true);
+
+        // Set session expiry time (60 seconds)
+        session.setMaxInactiveInterval(60);
+
+        // Session details
+        Date createTime = new Date(session.getCreationTime());
+        Date lastAccessTime = new Date(session.getLastAccessedTime());
+        String sessionId = session.getId();
+
+        // Visit count
+        Integer visitCount = (Integer) session.getAttribute("visitCount");
+
+        if (visitCount == null) {
+            visitCount = 1;
+        } else {
+            visitCount++;
+        }
+
+        session.setAttribute("visitCount", visitCount);
+
+        // Response
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h2>Session Tracking Information</h2>");
+        out.println("<p><b>Session ID:</b> " + sessionId + "</p>");
+        out.println("<p><b>Creation Time:</b> " + createTime + "</p>");
+        out.println("<p><b>Last Access Time:</b> " + lastAccessTime + "</p>");
+        out.println("<p><b>Visit Count:</b> " + visitCount + "</p>");
+        out.println("<p><b>Note:</b> Session expires in 60 seconds</p>");
+        out.println("<a href='SessionTracker'>Refresh</a>");
+        out.println("</body></html>");
+
+        out.close();
+    }
+}
